de62b9ac9b2549fd57c100c56ba01a30dc531358
abhilash <abhilashreddy340@gmail.com>
2022-09-22 19:45:50 +0530

    restructured application

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -11882,7 +11882,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58562" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54348" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
